006f68fea515e62522141d7b01c4351dd30dc1e1
liudiepie <tos975970836@gmail.com>
2022-08-01 11:51:22 -0400

    update

--- rgb_video_subscriber.cpp
+++ rgb_video_subscriber.cpp
@@ -23,6 +23,8 @@ class SegmentationSub : public rclcpp::Node{
 	: Node("rgb_subscriber_node"){
 	    subscription_1 = this->create_subscription<sensor_msgs::msg::Image>("color/video/image", 10, bind(&SegmentationSub::rgbCallback, this, _1));
 	    subscription_2 = this->create_subscription<sensor_msgs::msg::Image>("stereo/depth", 10, bind(&SegmentationSub::depthCallback, this, _1));
+	    publisher_ = this->create_publisher<sensor_msgs::msg::Image>("segmentation", 10);
+	    timer_ = this->create_wall_timer(500ms, bind(&SegmentationSub::pub_callback, this));
 	}
     private:
 
@@ -65,10 +67,12 @@ class SegmentationSub : public rclcpp::Node{
 
 		//resize the images
 		resize(rgbImage, rgbImage, Size(), 0.75, 0.75);
-		resize(img_dilation, img_dilation, Size(), 0.75, 0.75);
+		resize(img_dilation, green_binary, Size(640, 480));
+
+		green_binary.convertTo(green_binary, CV_8UC1);
 
 		imshow("original image", rgbImage);
-        	imshow("green filter", img_dilation);
+        	imshow("green filter", green_binary);
 		waitKey(30);
     	    }
     	    catch(cv_bridge::Exception& e){
@@ -82,28 +86,97 @@ class SegmentationSub : public rclcpp::Node{
 		double min;
 		double max;
 		minMaxLoc(depthImage, &min, &max);
-		//int max_int = ceil(max);
-		//Mat win_mat(Size(depthImage.cols, depthImage.rows), CV_8UC1, Scalar(0,0,0));
-		depthImage.convertTo(depthImage, CV_8UC3, 255 / (max-min), -255 * min / (max-min));
-		//depthImage.convertTo(depthImage, CV_8UC3);
-		Mat falseColor; // size (640,480)
-		applyColorMap(depthImage, falseColor, COLORMAP_JET);
-		imshow("depth image", falseColor);
+
+		//convert depth image into 0-255 information
+		//depthImage.convertTo(depthImage, CV_8UC1, 255 / (max-min), -255 * min / (max-min));
+		convertScaleAbs(depthImage, depthImage, 255/max);
+		Mat falseColor, cdimg, element; // size (640,480)
+		applyColorMap(depthImage, cdimg, COLORMAP_JET);
+		threshold(depthImage, falseColor, 100, 255, CV_THRESH_BINARY|CV_THRESH_OTSU);
+
+		//erosion and dilation
+		element = getStructuringElement(MORPH_RECT, Size(7,7), Point(3,3));
+		erode(falseColor, falseColor, element);
+		dilate(falseColor, depth_binary, element);
+		
+		//show the depth image
+		imshow("depth image", depth_binary);
 	    }
 	    catch(cv_bridge::Exception& e){
 		std::cout<<"error"<<std::endl;
 	    }
 	}
 
+	void pub_callback(){
+	    cv_bridge::CvImagePtr cv_ptr;
+	    
+	    Mat comb, proc, element;
+	    
+	    //switch the content of depth_binary and green_binary either 0 or 255 
+	    for(int i=0; i<green_binary.rows; ++i){
+		    for(int j=0; j<green_binary.cols; ++j){
+			    if(depth_binary.at<int>(i,j) != 0) depth_binary.at<int>(i,j) = 255;
+			    if(green_binary.at<int>(i,j) != 0) green_binary.at<int>(i,j) = 255;
+		    }
+	    }
+	   
+	    //combine depth_binary and green_binary 
+	    addWeighted(depth_binary, 0.5, green_binary, 0.5, 0.0, comb);
+	    
+	    proc = comb.clone();
+
+	    //gaussian blur 
+	    blur(proc, proc, Size(3,3));
+
+	    //dilate the image to reduce the noise
+	    element = getStructuringElement(MORPH_RECT, Size(5,5), Point(2,2));
+	    dilate(proc, proc, element);
+	    
+	    //canny to do edge detection 
+	    Canny(proc, proc, 100, 200, 3, false);
+
+	    //find the contours of the image
+	    vector<vector<Point>> contours;
+	    vector<Vec4i> hierarchy;
+	    findContours(proc, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+	    
+	    //draw the contours on the combined image
+	    cvtColor(comb, comb, COLOR_GRAY2BGR);
+	    drawContours(comb, contours, -1, Scalar(0, 255, 0), 2);
+
+	    /*
+	    cvtColor(cdimg, cdimg, COLOR_BGR2GRAY);
+	    blur(cdimg, cdimg, Size(3,3));
+	    Canny(cdimg, cdimg, 100, 200, 3, false);
+
+	    vector<vector<Point>> contours1;
+	    vector<Vec4i> hierarchy1;
+	    findContours(cdimg, contours1, hierarchy1, RETR_TREE, CHAIN_APPROX_NONE);
+
+	    drawContours(comb, contours1, -1, Scalar(0, 0, 255), 2);
+	    */
+	    
+	    //publish bgr result image
+	    sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", comb).toImageMsg();
+
+	    publisher_->publish(*msg.get());
+	}
+
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_1;
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_2;
+	rclcpp::TimerBase::SharedPtr timer_;
+
+	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+
+	size_t count_;
+
+	Mat depth_binary, green_binary, cdimg;
 };
 
 int main(int argc, char** argv){
 
     rclcpp::init(argc, argv);
     //node = rclcpp::Node::make_shared("rgb_subscriber_node");
-    std::cout<<"subscribe"<<std::endl; 
     //rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub = node->create_subscription<sensor_msgs::msg::Image>("color/video/image", 10, &rgbCallback);
     //auto sub = node->create_subscription<sensor_msgs::msg::Image>("rgb_image", 10, &rgbCallback);
     //rclcpp::spin(node);
